2383d8eb2cff127347d7b2df5020e3c4b260d7d6
Cesar-MLW <cesarmlwestphal@gmail.com>
2018-11-05 12:16:56 -0200

    Made them spanw in each swaure randomly

--- RobotGame_GameLab/Assets/Scripts/Game Manager/GameManager.cs
+++ RobotGame_GameLab/Assets/Scripts/Game Manager/GameManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-
+	private float[] SpawnPointsX = {2.58f, 3.38f, 1.61f};
+	private float[] SpawnPointsY = {-2.6f, -1.75f, -0.74f, 0.21f, 1.01f};
 	public GameObject[] parts;
 
 	public GameObject player;
@@ -15,7 +16,7 @@ public class GameManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.LeftControl)){
-			var part = Instantiate(parts[0], new Vector3(2.58f, 0.29f, 0.05f), new Quaternion(0, 0, 0, 1));
+			var part = Instantiate(parts[0], new Vector3(SpawnPointsX[Random.Range(0, SpawnPointsX.Length - 1)], SpawnPointsY[Random.Range(0, SpawnPointsY.Length - 1)], 0.05f), new Quaternion(0, 0, 0, 1));
 			part.transform.parent = player.transform;
 		}
 	}

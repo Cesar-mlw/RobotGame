a542deca484d278255562bb43f80d8be68703b09
Cesar Moura Leite Westphal <cesarmlwestphal@gmail.com>
2018-11-08 17:38:27 -0200

    Made Parts stop statically

--- RobotGame_GameLab/Assets/Scripts/Conveyor/Conveyor.cs
+++ RobotGame_GameLab/Assets/Scripts/Conveyor/Conveyor.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Conveyor : MonoBehaviour {
 	public GameManager gm;
 	private List<GameObject> spwnedParts = new List<GameObject>();
+
+	private float stoppingPoint = -2.33f;
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating("MovePartsOnConveyor", 1.0f, 2.0f);
@@ -13,15 +15,25 @@ public class Conveyor : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		spwnedParts = gm.SpwnedParts;
+		StopParts();
 	}
 
 	void MovePartsOnConveyor(){
 		if(spwnedParts.Count >= 0){
 			foreach (var item in spwnedParts){
-				if(item.transform.position.y - 1.19f >= -2.33f){
-					item.GetComponent<Rigidbody2D>().velocity = (transform.up * -1) * 0.3f;
-				}
+				item.GetComponent<Rigidbody2D>().velocity = (transform.up * -1) * 0.3f;
 			}
+
+		}
+	}
+
+	void StopParts(){
+		float stoppingPoint = -2.33f;
+		foreach (var item in spwnedParts){
+			if(item.transform.position.y <= stoppingPoint){
+				item.GetComponent<Rigidbody2D>().velocity *= 0;
+			}
+			stoppingPoint += 1.093f;
 		}
 	}
 }

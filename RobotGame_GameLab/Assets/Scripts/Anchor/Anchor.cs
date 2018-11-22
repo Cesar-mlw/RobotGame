99289a939870c5c6e2b1444edf1add8f1a8c671f
Cesar Moura Leite Westphal <cesarmlwestphal@gmail.com>
2018-11-22 17:21:24 -0200

    Added anchors for player to put part over

---
+++ RobotGame_GameLab/Assets/Scripts/Anchor/Anchor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anchor : MonoBehaviour {
+
+	private bool occupied = false;
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	public bool Occupied{
+		get{
+			return occupied;
+		}
+		set{
+			occupied = value;
+		}
+	}
+}
